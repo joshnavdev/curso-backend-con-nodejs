31b23fe88751c5c43e48ba41abe60c13543b6c99
Joshua Navarro <joshua.navarro35@gmail.com>
2024-10-02 18:56:56 -0500

    add assignation by value and reference

--- conceptos-basicos/ecmascript/let-const.js
+++ conceptos-basicos/ecmascript/let-const.js
@@ -1,23 +1,41 @@
 //////////////////////////////////////////////////
 // let
-let name = 'Joshua';
-console.log(name); // Joshua
-name = 'Nicolas';
-console.log(name); // Nicolas
+// let name = 'Joshua';
+// console.log(name); // Joshua
+// name = 'Nicolas';
+// console.log(name); // Nicolas
 
 //////////////////////////////////////////////////
 // const
-const lastname = 'Navarro';
-console.log(lastname);
-lastname = 'Ruiz'; // TypeError: Assignment to constant variable.
+// const lastname = 'Navarro';
+// console.log(lastname);
+// lastname = 'Ruiz'; // TypeError: Assignment to constant variable.
 
 //////////////////////////////////////////////////
 // var
-console.log(address); // undefined
-var address = 'Lima';
-console.log(address); // address
+// console.log(address); // undefined
+// var address = 'Lima';
+// console.log(address); // address
 
 // output
 // var address;
 // console.log(address);
 // address = 'Lima';
+
+// Pass by value vs Pass by reference
+// let a = 10;
+// let b = a;
+
+// b = 12;
+// console.log(a, b);
+
+// const c = { name: 'Joshua' };
+// const d = c;
+
+// console.log(c, d);
+
+// d.name = 'Nicolas';
+
+// console.log(c, d);
+
+// d.name = 'Nicolas';
