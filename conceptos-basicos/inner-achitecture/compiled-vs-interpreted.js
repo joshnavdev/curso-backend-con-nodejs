117ba0a2fbf36dbbbe72d91a9e653ebbfa98b713
Joshua Navarro <joshua.navarro35@gmail.com>
2024-10-03 17:47:33 -0500

    add inner architecture examples

---
+++ conceptos-basicos/inner-achitecture/compiled-vs-interpreted.js
@@ -0,0 +1,19 @@
+// Compilado -> Unexpected token error
+
+// console.log('Aprendiendo NodeJS');
+// console.log('Es JS interpretado?');
+// console.log('Comprobemoslo);
+
+// Compilado -> Hoisting
+
+// saludar();
+
+// function saludar() {
+//   console.log('Hola!!');
+// }
+
+// Interpretado
+console.log('Probando si es interpretado');
+setTimeout(() => {
+  console.log(MyBackendProject);
+}, 5000);
